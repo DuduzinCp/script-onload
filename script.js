7dcdaa203dd5be2ec27ce46e130788fbd22cb621
DuduzinCp <102256972+DuduzinCp@users.noreply.github.com>
2024-06-24 10:29:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
+  console.log('DOM completamente carregado e analisado');
+  
   const addToCartButton = document.querySelector('button[name="add"]');
   if (addToCartButton) {
+    console.log('Botão "Adicionar ao carrinho" encontrado');
+    
     const helloWorldText = document.createElement('div');
     helloWorldText.innerText = 'Olá mundo';
     addToCartButton.parentNode.insertBefore(helloWorldText, addToCartButton);
+  } else {
+    console.log('Botão "Adicionar ao carrinho" não encontrado');
   }
 });
